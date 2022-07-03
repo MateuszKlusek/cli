aedbb928d2d635398b66de871c3f1c21070f8b48
Mateusz Kłusek <mateuszklusek@outlook.com>
2022-07-03 21:59:17 +0200

    small syntax refactoring and cleaning up content

--- cc.sh
+++ cc.sh
@@ -1,33 +1,42 @@
 cc(){
+    # syntax -> cc component_name [tsx|typescript|ts]
+
+    # posible arguments (other arguments will be ommited)
     arr=("tsx" "typescript" "ts")
+    # default extension
+    extention=jsx
     if [ "$#" -ne  1 ]; then
-            extention=jsx
+            # if arguments in arr, we have a tsx files
             if [[ " ${arr[*]} " =~ " $2 " ]]; then
                     extention=tsx
             fi
-    else
-            extention=jsx
     fi
     mkdir $1
     cd $1
+
+    # create a component file with a proper extention and populate file with content
     touch "$1.$extention"
     cat > "$1.$extention" << EOF
-    const $1 = () => {
-return <div></div>;
-};
-export default $1;
+        const $1 = () => {
+    return <div></div>;
+    };
+    export default $1;
 EOF
 
-        touch "$1.styled.$extention"
-        cat > "$1.styled.$extention" << OEF
-import styled from "styled-components"
+    # create styles component files 
+    touch "$1.styled.$extention"
+    cat > "$1.styled.$extention" << OEF
+    import styled from "styled-components"
 OEF
-        touch "$1.test.$extention"
-        cat > "$1.test.$extention" << OEF
-import {render, screen} from '@testing-library/react';
-import '@testing-library/jest-dom'2
-// component
-import $1 from "./$1"5
-test.todo('add tests');
+
+    # creating style file with default todo test
+    touch "$1.test.$extention"
+    cat > "$1.test.$extention" << OEF
+    import {render, screen} from '@testing-library/react';
+    import '@testing-library/jest-dom'2
+    // component
+    import $1 from "./$1"5
+    test.todo('add tests');
 OEF
+
 }
